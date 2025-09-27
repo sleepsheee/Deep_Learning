bf902b996b9b05f98916eadf114cf9ccbe3c3d5f
sleepsheee <sleepshee29@gmail.com>
2025-09-27 22:55:48 +0800

    Basic KD

--- KD_Basic.py
+++ KD_Basic.py
@@ -1,25 +1,48 @@
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
+import numpy as np
 import os
 import json
 from open_clip.factory import create_model_and_transforms
 from torchvision import datasets, transforms
 import torchvision.models as models
-from open_clip import get_tokenizer, get_model_config, get_cast_dtype, build_zero_shot_classifier
-from torch.utils.data import DataLoader, Dataset
+from open_clip import get_tokenizer, get_model_config, get_cast_dtype
+from torch.utils.data import DataLoader
 
-import random
+'''
+An example for knowledge distillation with CLIP model as teacher and ResNet18 as student on CIFAR100 dataset.
+Teacher: CLIP ViT-B-16 + classification head
+Student: ResNet18 + classification head
+'''
 
-class DataLoad():
+class KD_Basic():
     def __init__(self):
+        # data loader
         self.train_loader = None
         self.test_loader = None
+
+        self.preprocess_train = None
+        self.preprocess_test = None
+
+        # clip metadata 
         self.catalog = None
         self.all_templates = None
         self.all_labels = None
 
-    def load_metadata(self, metadir="clipeval"):
+        self.student_model = None
+        self.teacher_model = None
+        self.classifier_head = None
+
+    # 设置随机种子
+    def seed_it(self, seed):
+        os.environ["PYTHONSEED"] =  str(seed)
+        np.random.seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        # 设置全局随机数种子
+        torch.manual_seed(seed)
+
+    def load_metadata(self, metadir="../KD_codeV3/clipeval"):
         with open(os.path.join(metadir, 'dataset_catalog.json')) as f:
             self.catalog = json.load(f)
 
@@ -38,7 +61,7 @@ class DataLoad():
                                     (0.26862954, 0.26130258, 0.27577711)),
             ])
 
-        train_set = datasets.CIFAR100(root="./data", download=True, train=True, transform=train_transform)
+        train_set = datasets.CIFAR100(root="../KD_codeV3/data", download=True, train=True, transform=self.preprocess_train)
         self.train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
         test_transform = transforms.Compose([
@@ -48,71 +71,112 @@ class DataLoad():
                                 (0.26862954, 0.26130258, 0.27577711)),
         ])
 
-        test_set = datasets.CIFAR100(root="./data", download=True, train=False, transform=test_transform)
+        test_set = datasets.CIFAR100(root="../KD_codeV3/data", download=True, train=False, transform=test_transform)
         self.test_loader = DataLoader(test_set, batch_size=batch_size // 2, shuffle=False)
 
 
 
-    '''
+    '''  
     model.encode_image(image: Tensor)
-Given a batch of images, returns the image features encoded by the vision portion of the CLIP model.
+    Given a batch of images, returns the image features encoded by the vision portion of the CLIP model.
 
-model.encode_text(text: Tensor)
-Given a batch of text tokens, returns the text features encoded by the language portion of the CLIP model.
+    model.encode_text(text: Tensor)
+    Given a batch of text tokens, returns the text features encoded by the language portion of the CLIP model.
 
-model(image: Tensor, text: Tensor)
-Given a batch of images and a batch of text tokens, returns two Tensors, containing the logit scores corresponding to each image and text input. The values are cosine similarities between the corresponding image and text features, times 100.
+    model(image: Tensor, text: Tensor)
+    Given a batch of images and a batch of text tokens, returns two Tensors, containing the logit scores corresponding to each image and text input. The values are cosine similarities between the corresponding image and text features, times 100.
     '''
 
     def load_clip_model_from_local(self, model_name, weights_path, device):
-        # model_cfg = get_model_config(model_name)
-        # dtype = get_cast_dtype("fp32")
+        model_cfg = get_model_config(model_name)
+        dtype = get_cast_dtype("fp32")
         model, preprocess_train, preprocess_val = create_model_and_transforms(
             model_name=model_name,
-            pretrained=None,
+            pretrained=weights_path,
             device=device)
         '''
         当你传入 "openai" 或 "laion400m_e32" 时，函数会 自动下载并加载对应预训练权重
         如果传入 None，则 只初始化模型结构，不会加载任何权重, 模型的参数会按 PyTorch 默认初始化方式 初始化（通常是随机初始化）
         如果你想用本地 checkpoint（.pt 或 .bin），直接传文件路径不一定生效。更通用的方式是： 然后再手动 load_state_dict。
         '''
-        state_dict = torch.load(weights_path, map_location=device)
+        # state_dict = torch.load(weights_path, map_location=device)
         # 当使用 torch.nn.DataParallel 或多GPU训练时，PyTorch会自动给参数key添加"module."前缀单GPU模型没有这个前缀，所以需要移除才能正确加载
-        if any(key.startswith("module.") for key in state_dict.keys()):
-            state_dict = {key.replace("module.", ""): value for key, value in state_dict.items()} 
-        model.load_state_dict(state_dict)
+        # if any(key.startswith("module.") for key in state_dict.keys()):
+        #     state_dict = {key.replace("module.", ""): value for key, value in state_dict.items()} 
+        # model.load_state_dict(state_dict)
         # 禁用dropout、batch norm使用运行时的统计量
         model.eval()
         # model: 加载好权重的CLIP模型
         # preprocess_val: 验证/推理用的图像预处理流程
-        return model, preprocess_val
+        self.preprocess_train = preprocess_train
+        self.preprocess_test = preprocess_val
+        self.teacher_model = model
+
+        
+    def accuracy(self, output, target, topk=(1,)):
+        pred = output.topk(max(topk), 1, True, True)[1].t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+        return [float(correct[:k].reshape(-1).float().sum(0, keepdim=True).cpu().numpy()) for k in topk]
+    
+    def evaluate_accuracy(self, model, images, labels, device):
+        """Evaluate accuracy using the student model's classification head."""
+        top1, top5, total = 0., 0., 0.
+
+        # Set the model to evaluation mode (no gradients)
+        model.eval() 
+        images = images.to(device)
+        labels = labels.to(device)
+
+        with torch.no_grad(), torch.cuda.amp.autocast():
+        # Iterate over the test data
+            # Forward pass through the student model
+            image_features = model(images)  # The student model should output image features
+
+            # Optionally normalize the features if necessary (depending on your model's architecture)
+            image_features = F.normalize(image_features, dim=-1)
+
+            # Pass the image features through the classification head to get logits
+            logits = self.classifier_head(image_features)
+
+            # Compute the accuracy based on the classification head's output (logits)
+            acc1, acc5 = self.accuracy(logits, labels, topk=(1, 5))
+
+            top1 += acc1
+            top5 += acc5
+            total += images.size(0)
+
+        # Calculate top-1 and top-5 accuracy
+        top1 = top1 / total
+        top5 = top5 / total
+
+        return top1, top5
     
     def run(self):
-        self.load_metadata()
-        self.get_cifar100_dataloaders()
-
+        '''
+        cpu or gpu
+        '''
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         print(device)
 
+        local_weights_path = "../KD_codeV3/clip_models/DFN2B-CLIP-ViT-B-16/open_clip_pytorch_model_1.bin"
+        self.load_clip_model_from_local("ViT-B-16", local_weights_path, device=device)
+        tokenizer = get_tokenizer("ViT-B-16")
+
+        self.get_cifar100_dataloaders()
 
         print(f"train batches num: {len(self.train_loader)}")
         print(f"test batches num: {len(self.test_loader)}")
 
-        self.load_metadata(metadir="clipeval")
-        templates = self.all_templates['CIFAR100']
-        classnames = self.all_labels['CIFAR100']
+        self.load_metadata(metadir="../KD_codeV3/clipeval")
+        # templates = self.all_templates['CIFAR100']
+        # classnames = self.all_labels['CIFAR100']
         num_classes = 100
-
-        
-        local_weights_path = "clip_models\DFN2B-CLIP-ViT-B-16\open_clip_pytorch_model_1.bin"
-        teacher_model, preprocess = self.load_clip_model_from_local("ViT-B-16", local_weights_path, device=device)
-        tokenizer = get_tokenizer("ViT-B-16")
 
         student_model = ResNet18FeatureExtractor(weights=None)
         student_model.to(device)
-        teacher_model.to(device)
+        self.teacher_model.to(device)
         student_model.eval()
-        teacher_model.eval()
+        self.teacher_model.eval()
         
         for i, (images, targets) in enumerate(self.train_loader):
             if i == 0:
@@ -121,6 +185,7 @@ Given a batch of images and a batch of text tokens, returns two Tensors, contain
                 target = targets[0]
                 image = images[0]
                 image = image.to(device)
+                # print(f"image after preprocess_train: {image_preocessed}, image_preocessed shape: {image_preocessed.shape}")
                 print(f"target: {target}, image shape: {image.shape}")
                 # targets_text = random.choice(templates).format(classnames[target])
                 # print(f"targets_text:", targets_text)
@@ -131,10 +196,11 @@ Given a batch of images and a batch of text tokens, returns two Tensors, contain
                 # 使用unsqueeze函数在第0个维度上添加一个大小为1的维度
                 image = image.unsqueeze(0)
                 student_image_features = student_model(image)
-                classifier_head = ClassificationHead(512, num_classes).to(device)
-                student_logits_classification = classifier_head(student_image_features)
-                teacher_image_features = teacher_model.encode_image(image)
-                teacher_logits_classification = classifier_head(teacher_image_features)
+                target = target.unsqueeze(0)
+                self.classifier_head = ClassificationHead(512, num_classes).to(device)
+                student_logits_classification = self.classifier_head(student_image_features)
+                teacher_image_features = self.teacher_model.encode_image(image)
+                teacher_logits_classification = self.classifier_head(teacher_image_features)
 
                 print(f"student_image_features shape: {student_image_features.shape}")
                 print(f"student_logits_classification shape: {student_logits_classification.shape}")
@@ -148,8 +214,13 @@ Given a batch of images and a batch of text tokens, returns two Tensors, contain
                 student_index = torch.argmax(student_probabilities)
                 teacher_index = torch.argmax(teacher_probabilities)
                 # print("概率之和:", logits_classification.sum(dim=1))  
-                print(f"student最大值索引:", student_index.item())
-                print(f"teacher最大值索引:", teacher_index.item())
+                print(f"student model 最大值索引:", student_index.item())
+                print(f"teacher model 最大值索引:", teacher_index.item())
+
+
+
+                top1, top5 = self.evaluate_accuracy(student_model, image, target, device)
+                print(f"Top-1 Accuracy: {top1*100:.2f}%, Top-5 Accuracy: {top5*100:.2f}%")
                 break
 
 
@@ -187,6 +258,25 @@ class ClassificationHead(nn.Module):
     
 
 if __name__ == "__main__":
-    dataload = DataLoad()
-    dataload.run()
+    kd = KD_Basic()
+    kd.seed_it(42)
+    kd.run()
     
+
+
+'''
+Files already downloaded and verified
+Files already downloaded and verified
+cuda
+train batches num: 391
+test batches num: 157
+WARNING:root:No pretrained weights loaded for model 'ViT-B-16'. Model initialized randomly.
+i: 0, images shape: torch.Size([128, 3, 224, 224]), targets shape: torch.Size([128])
+target: 68, image shape: torch.Size([3, 224, 224])
+student_image_features shape: torch.Size([1, 512])
+student_logits_classification shape: torch.Size([1, 100])
+teacher_image_features: torch.Size([1, 512])
+teacher_logits_classification shape: torch.Size([1, 100])
+student最大值索引: 59
+teacher最大值索引: 48
+'''
